dc05ad21be71614383e7a0a87aa8732c9fcd280b
巫向阳 <wuxyboy@sina.com>
2020-11-02 20:49:49 +0800

    完善配置6

---
+++ src/utils/promisify.js
@@ -0,0 +1,36 @@
+function wxPromisify(functionName,params){
+    return new Promise((resolve,reject)=>{
+        wx[functionName]({
+            ...params,
+            success:res=>resolve(res),
+            fail:err=>reject(err)
+        })
+    })
+}
+
+/**
+ * 登录
+ */
+function login(params={}) {
+    return wxPromisify('login', params);
+  }
+  
+  /**
+   * 获取用户信息
+   */
+  function getUserInfo(params={}) {
+    return wxPromisify('getUserInfo', params);
+  }
+  
+  /**
+   * 获取用户权限
+   */
+  function getSetting(params={}) {
+    return wxPromisify('getSetting', params);
+  }
+  
+  module.exports = {
+    login,
+    getUserInfo,
+    getSetting
+  }
